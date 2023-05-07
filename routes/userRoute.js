dd2d95697d42a661ed98a49be5c5e2b817432e25
GiacomoDiFa <giacomodfabrizio@gmai.com>
2023-05-07 17:56:47 +0200

    add some server functionality

--- routes/userRoute.js
+++ routes/userRoute.js
@@ -1,27 +1,130 @@
 const express = require('express');
 const router = express.Router();
+const uuid = require('uuid');
 
 const User = require('../models/User');
 
-router.post('/verifyadmin', async(req,res)=>{
-    const {email,password} = req.body;
-    try{
-        const user = await User.findOne({email:email, password:password});
-        if(user){
-            const temp = {
-                email : user.email,
-                isAdmin : user.isAdmin,
-                _id : user._id,
-            }
-            res.send(temp);
-        }
-        else{
-            return res.status(404).json({message: 'User is not admin'});
-        }
+class Session{
+    constructor(username, expiresAt){
+        this.username = username
+        this.expiresAt = expiresAt
     }
-    catch(error){
-        return res.status(404).json({error});
+
+    isExpired(){
+        this.expiresAt < (new Date())
     }
+}
+
+const sessions = {}
+
+router.post('/signin', async(req,res)=>{
+    /*modificare mettendo il fatto che vedo l'utente da database admin*/
+    const {username, password} = req.body;
+    if(!username){
+        res.status(401).end()
+        return
+    }
+    const user = await User.findOne({email:username});
+    const expectedPassword = user.password;
+    if(!expectedPassword || expectedPassword !== password){
+        res.status(401).end()
+        return
+    }
+
+    const sessionToken = uuid.v4();
+
+    const now = new Date();
+    const expiresAt = new Date(+now+120*1000);
+
+    const session = new Session(username,expiresAt);
+    sessions[sessionToken] = session;
+    
+    res.cookie("session_token",sessionToken,{expires:expiresAt});
+    res.end();
+});
+
+router.get('/welcome', async(req,res)=>{
+    if(!req.cookies){
+        res.status(401).end();
+        return
+    }
+
+    const sessionToken = req.cookies['session_token'];
+    if(!sessionToken){
+        res.status(401).end();
+        return
+    }
+    userSession = sessions[sessionToken];
+    if(!userSession){
+        res.status(401).end();
+        return
+    }
+    if(userSession.isExpired()){
+        delete sessions[sessionToken];
+        res.status(401).end();
+        return
+    }
+    res.send(`Welcome ${userSession.username}!`).end();
+});
+
+router.post('/refresh', async(req,res)=>{
+    if (!req.cookies) {
+        res.status(401).end();
+        return
+    }
+
+    const sessionToken = req.cookies['session_token'];
+    if (!sessionToken) {
+        res.status(401).end();
+        return
+    }
+
+    userSession = sessions[sessionToken]
+    if (!userSession) {
+        res.status(401).end();
+        return
+    }
+    if (userSession.isExpired()) {
+        delete sessions[sessionToken];
+        res.status(401).end();
+        return
+    }
+    // (END) The code until this point is the same as the first part of the welcomeHandler
+
+    // create a new session token
+    const newSessionToken = uuid.v4();
+
+    // renew the expiry time
+    const now = new Date();
+    const expiresAt = new Date(+now + 120 * 1000);
+    const session = new Session(userSession.username, expiresAt);
+
+    // add the new session to our map, and delete the old session
+    sessions[newSessionToken] = session;
+    delete sessions[sessionToken];
+
+    // set the session token to the new value we generated, with a
+    // renewed expiration time
+    res.cookie("session_token", newSessionToken, { expires: expiresAt });
+    res.end();
+});
+
+router.get('logout',async(req,res)=>{
+    if (!req.cookies) {
+        res.status(401).end();
+        return
+    }
+
+    const sessionToken = req.cookies['session_token'];
+    if (!sessionToken) {
+        res.status(401).end();
+        return
+    }
+    
+    delete sessions[sessionToken];
+
+    res.cookie("session_token", "", { expires: new Date() });
+    res.end();
 });
 
 module.exports = router;
--- server/server.js
+++ server/server.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const cors = require('cors');
+const bodyParser = require('body-parser');
+const cookieParser = require('cookie-parser');
 const app = express();
 
 const dbConfig = require('./db');
@@ -8,6 +10,9 @@ const postRoute = require('../routes/postRoute');
 
 app.use(cors());
 app.use(express.json());
+app.use(bodyParser.json());
+app.use(cookieParser());
+
 app.use('/api/user',userRoute);
 app.use('/api/post',postRoute);
 
